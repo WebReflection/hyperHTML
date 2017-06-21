b6af7885fa1bd8c1afab6214c3b44af2c1e20410
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-06-21 23:55:06 +0100

    hyperHTML fully refactored
      - compartible by default with Firefox < 55
      - improved IE compatibility
      - 2X better performance than previous version
      - avoided any redundant operation
      - new algorithm to map node once per template literal
      - better memory management
      - extremely improved .adopt(el) reliability
      - adopted nodes share same pre-parsed literal of bound one
      - improved virtual adoption test coverage
    
    TODO:
      - hyperHTML.wire(any?, 'adopt')

--- test/runner.js
+++ test/runner.js
@@ -7,11 +7,20 @@ require('jsdom').env(
     global.hyperHTML = require('../hyperhtml.js');
     require('./test.js');
     setTimeout(function () {
+      var proto = global.document;
+      do {
+        proto = Object.getPrototypeOf(proto);
+      } while(!Object.getOwnPropertyNames(proto, 'children'));
+
+      delete proto.children;
+
+      global.navigator = {userAgent: 'Firefox/54'};
       Object.prototype.append = function () {
         [].forEach.call(arguments, this.appendChild, this);
       };
       Object.prototype.nodeType = 2;
-      global.String.prototype.trim = global.WeakMap = void 0;
+      var $Map = global.Map;
+      global.String.prototype.trim = global.WeakMap = global.Map = void 0;
       delete require.cache[require.resolve('../hyperhtml.js')];
       delete require.cache[require.resolve('./test.js')];
       // fake initial feature detection
@@ -21,12 +30,16 @@ require('jsdom').env(
         global.document.createElement = function (name) {
           return createElement.call(
             global.document,
-            name === 'template' ? (++templates % 2 ? 'div' : name) : name);
+            name === 'template' ? (++templates % 15 ? 'div' : name) : name);
         };
-        //delete global.document.createElement('template').constructor.prototype.content;
+        //delete document.createElement('template').constructor.prototype.content;
         return {firstChild:{attributes:[{name:'class'}]}};
       };
+      var isArray = Array.isArray;
+      delete Array.isArray;
       global.hyperHTML = require('../hyperhtml.js');
+      Array.isArray = isArray;
+      Map = $Map;
       require('./test.js');
     }, 500);
   }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
 tressa.title('HyperHTML');
 tressa.assert(typeof hyperHTML === 'function', 'hyperHTML is a function');
 
+/*
+tressa.async(function (done) {
+  var wrap = document.createElement('p');
+  hyperHTML.bind(wrap)`${1}`;
+  hyperHTML.bind(wrap)`${2}`;
+  tressa.assert(wrap.textContent == 2);
+  done();
+})
+*/
 tressa.async(function (done) {
   tressa.log('## injecting text and attributes');
   var i = 0;
@@ -321,6 +330,27 @@ tressa.async(function (done) {
   }
 })
 .then(function () {
+  tressa.log('## weird .adopt(node) cases');
+  let wrap = document.createElement('div');
+  wrap.innerHTML = '<div>text</div>';
+  hyperHTML.adopt(wrap)`<div> ${'right'}</div>`;
+  tressa.assert(wrap.innerHTML === '<div> right</div>', 'right text OK');
+  hyperHTML.adopt(wrap)`<div>${'left'} </div>`;
+  tressa.assert(wrap.innerHTML === '<div>left </div>', 'left text OK');
+  wrap.innerHTML = '';
+  hyperHTML.adopt(wrap)`${'any'}`;
+  tressa.assert(wrap.innerHTML === 'any', '`${\'virtual\'}` is like `${\'any\'}`');
+  wrap.innerHTML = '<br>';
+  hyperHTML.adopt(wrap)`<br>${'virtual'}`;
+  tressa.assert(/^<br>virtual<!--.+?-->$/.test(wrap.innerHTML), '`<br>${\'virtual\'}`');
+  wrap.innerHTML = '<hr>';
+  hyperHTML.adopt(wrap)`${'virtual'}<hr>`;
+  tressa.assert(/^virtual<!--.+?--><hr>$/.test(wrap.innerHTML), '${\'virtual\'}<hr>`');
+  wrap.innerHTML = '<br><i>before</i><hr>';
+  hyperHTML.adopt(wrap)`<br>${'<strong>after</strong>'}<hr>`;
+  tressa.assert(/^<br><strong>after<\/strong><!--.+?--><hr>$/.test(wrap.innerHTML), '<br>${\'<strong>after</strong>\'}<hr>');
+})
+.then(function () {
   tressa.log('## for code coverage sake');
   let wrap = document.createElement('div');
   let result = hyperHTML.wire()`<!--not hyprHTML-->`;
@@ -358,6 +388,8 @@ tressa.async(function (done) {
   let onclick = (e) => {};
   let handler = {handleEvent: onclick};
   hyperHTML.bind(wrap)`<p onclick="${onclick}" onmouseover="${handler}" align="${'left'}"></p>`;
+  handler = {handleEvent: onclick};
+  hyperHTML.bind(wrap)`<p onclick="${onclick}" onmouseover="${handler}" align="${'left'}"></p>`;
   hyperHTML.bind(wrap)`<p onclick="${onclick}" onmouseover="${handler}" align="${'left'}"></p>`;
   hyperHTML.bind(wrap)`<br>${arr[0]}<br>`;
   hyperHTML.bind(wrap)`<br>${arr}<br>`;
@@ -372,7 +404,15 @@ tressa.async(function (done) {
   tressa.assert(!result.innerHTML, 'empty content');
   let tr = hyperHTML.wire()`<tr><td>ok</td></tr>`;
   tressa.assert(true, 'even TR as template');
+
+  hyperHTML.bind(wrap)` ${1} `;
+  tressa.assert(wrap.textContent === ' 1 ', 'text in between');
+
+  hyperHTML.bind(wrap)` <br>${1}</br> `;
+  tressa.assert(/ <br>1<!--_hyper_html:\s*[0-9-]+;--><br> /.test(wrap.innerHTML), 'virtual content in between');
+
 })
+// */
 .then(function () {
   if (!tressa.exitCode) {
     document.body.style.backgroundColor = '#0FA';
