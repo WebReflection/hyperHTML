d0096d4ce0602be2f39eeeb96ae37aeb847b3487
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-08-23 11:55:42 +0200

    Using latest tta module to fix Chrome issue

--- index.js
+++ index.js
@@ -1727,7 +1727,7 @@ var hyperHTML = (function (document) {
     var RAW = 'raw';
 
     var isBroken = function isBroken(UA) {
-      return /(Firefox|Safari)\/(\d+)/.test(UA) && !/(Chrom|Android)\/(\d+)/.test(UA);
+      return /(Firefox|Safari)\/(\d+)/.test(UA) && !/(Chrom[eium]+|Android)\/(\d+)/.test(UA);
     };
 
     var broken = isBroken((document.defaultView.navigator || {}).userAgent);
