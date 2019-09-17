23b2bf4f5c5cd45e9a3bbbac6f6d734d19c81f63
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-09-17 09:18:27 +0200

    updated to latest domdiff

--- index.js
+++ index.js
@@ -199,11 +199,8 @@ var hyperHTML = (function (document) {
     return i < length ? get(list[i], 0) : 0 < i ? get(list[i - 1], -0).nextSibling : before;
   };
   var remove = function remove(get, parent, children, start, end) {
-    if (end - start < 2) parent.removeChild(get(children[start], -1));else {
-      var range = parent.ownerDocument.createRange();
-      range.setStartBefore(get(children[start], -1));
-      range.setEndAfter(get(children[end - 1], -1));
-      range.deleteContents();
+    while (start < end) {
+      _removeChild(get(children[start++], -1), parent);
     }
   }; // - - - - - - - - - - - - - - - - - - -
   // diff related constants and utilities
@@ -422,6 +419,22 @@ var hyperHTML = (function (document) {
 
   var smartDiff = function smartDiff(get, parentNode, futureNodes, futureStart, futureEnd, futureChanges, currentNodes, currentStart, currentEnd, currentChanges, currentLength, compare, before) {
     applyDiff(OND(futureNodes, futureStart, futureChanges, currentNodes, currentStart, currentChanges, compare) || HS(futureNodes, futureStart, futureEnd, futureChanges, currentNodes, currentStart, currentEnd, currentChanges), get, parentNode, futureNodes, futureStart, currentNodes, currentStart, currentLength, before);
+  };
+
+  var _removeChild = function removeChild(child, parentNode) {
+    /* istanbul ignore if */
+    if ('remove' in child) {
+      _removeChild = function removeChild(child) {
+        child.remove();
+      };
+    } else {
+      _removeChild = function removeChild(child, parentNode) {
+        /* istanbul ignore else */
+        if (child.parentNode === parentNode) parentNode.removeChild(child);
+      };
+    }
+
+    _removeChild(child, parentNode);
   };
 
   /*! (c) 2018 Andrea Giammarchi (ISC) */
